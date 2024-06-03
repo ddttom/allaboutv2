1ce67faee472ba8db6716ca27691507665e8028b
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-06-03 11:53:37 +0200

    ### Subject: Update selector for text block rendering
    ### Body:
    Changed the CSS selector from `.text.block` to `.text-wrapper` in the `decorate` function to align with updated HTML structure. This adjustment ensures that text blocks are correctly rendered with the appropriate expressions, enhancing consistency and avoiding potential rendering issues.

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,5 +1,5 @@
 import { renderExpressions } from "/plusplus/plugins/expressions/src/expressions.js";
 
 export default function decorate(block) {
-  renderExpressions(document.querySelector(".text.block"));
+  renderExpressions(document.querySelector('.text-wrapper'));
 }
