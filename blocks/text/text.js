2b3e150c13faa515c544acf21d336f9793891b5e
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-06-03 11:48:27 +0200

    ### Simplify selector and improve element targeting
    Updated `renderExpressions` selector from a string to a more precise `document.querySelector` call targeting the `.text.block` element. This change enhances the accuracy and performance of element selection within the text block decoration functionality.

--- blocks/text/text.js
+++ blocks/text/text.js
@@ -1,5 +1,5 @@
 import { renderExpressions } from "/plusplus/plugins/expressions/src/expressions.js";
 
 export default function decorate(block) {
-    renderExpressions('div.text - wrapper');
+  renderExpressions(document.querySelector(".text.block"));
 }
