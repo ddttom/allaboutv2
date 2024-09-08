269b303bc2323c17655520eb2750c7cbfbc613da
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-09-08 19:25:44 +0100

    debug

--- blocks/imageslider/imageslider.js
+++ blocks/imageslider/imageslider.js
@@ -1,5 +1,11 @@
 export default async function decorate(block) {
+  // eslint-disable-next-line no-console
+  console.log('Initializing imageslider');
+
   const images = [...block.querySelectorAll('a')].map((a) => a.href);
+  // eslint-disable-next-line no-console
+  console.log('Images found:', images);
+
   const container = document.createElement('div');
   container.className = 'imageslider-container';
 
@@ -20,6 +26,8 @@ export default async function decorate(block) {
 
   function showImage(index) {
     imageElement.src = images[index];
+    // eslint-disable-next-line no-console
+    console.log('Showing image:', index, images[index]);
     updateIndicators();
   }
 
@@ -27,26 +35,36 @@ export default async function decorate(block) {
     indicators.innerHTML = images.map((_, i) => 
       `<span class="indicator ${i === currentIndex ? 'active' : ''}"></span>`
     ).join('');
+    // eslint-disable-next-line no-console
+    console.log('Indicators updated');
   }
 
   function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
     showImage(currentIndex);
+    // eslint-disable-next-line no-console
+    console.log('Next image shown');
   }
 
   function prevImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
     showImage(currentIndex);
+    // eslint-disable-next-line no-console
+    console.log('Previous image shown');
   }
 
   function startRotation() {
     if (!isHovering) {
       intervalId = setInterval(nextImage, 5000);
+      // eslint-disable-next-line no-console
+      console.log('Rotation started');
     }
   }
 
   function stopRotation() {
     clearInterval(intervalId);
+    // eslint-disable-next-line no-console
+    console.log('Rotation stopped');
   }
 
   // Shuffle images
@@ -54,6 +72,8 @@ export default async function decorate(block) {
     const j = Math.floor(Math.random() * (i + 1));
     [images[i], images[j]] = [images[j], images[i]];
   }
+  // eslint-disable-next-line no-console
+  console.log('Images shuffled:', images);
 
   showImage(currentIndex);
   startRotation();
@@ -61,19 +81,30 @@ export default async function decorate(block) {
   container.addEventListener('mouseenter', () => {
     isHovering = true;
     stopRotation();
+    // eslint-disable-next-line no-console
+    console.log('Mouse entered, rotation paused');
   });
 
   container.addEventListener('mouseleave', () => {
     isHovering = false;
     nextImage();
     startRotation();
+    // eslint-disable-next-line no-console
+    console.log('Mouse left, rotation resumed');
   });
 
   document.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowLeft') {
       prevImage();
+      // eslint-disable-next-line no-console
+      console.log('Left arrow pressed');
     } else if (e.key === 'ArrowRight') {
       nextImage();
+      // eslint-disable-next-line no-console
+      console.log('Right arrow pressed');
     }
   });
+
+  // eslint-disable-next-line no-console
+  console.log('Imageslider initialization complete');
 }
