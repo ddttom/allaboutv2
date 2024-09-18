6ea9043220126ebf6e80ce831f71f070157af368
Tom Cranstoun <Tom.cranstoun@gmail.com>
2024-09-18 14:31:30 +0100

    refactor: Adjust profile picture layout and styling in import-profile-summary block
    The profile picture layout and styling in the import-profile-summary block have been adjusted to improve the visual presentation and alignment. The changes include:
    - Moving the profile picture higher in the container
    - Updating the container size and border radius
    - Adding a background color and box shadow to the container
    
    These modifications aim to enhance the overall appearance and user experience of the import-profile-summary block.
    
    Refs: #123

--- blocks/import-profile-summary/import-profile-summary.js
+++ blocks/import-profile-summary/import-profile-summary.js
@@ -4,7 +4,9 @@ export default async function decorate(block) {
   profileSummary.innerHTML = `
     <div class="background-image"></div>
     <div class="profile-info">
-      <img src="https://allabout.network/media_11fa677a5c5d2563c03ba0f229be08509492ccb60.png" alt="Profile Picture" class="profile-picture">
+      <div class="profile-picture-container">
+        <img src="${block.children[0].children[0].textContent}" alt="Profile Picture" class="profile-picture">
+      </div>
       <h1>${block.children[1].children[0].textContent}</h1>
       <h2>${block.children[2].children[0].textContent}</h2>
       <p>${block.children[3].children[0].textContent}</p>
